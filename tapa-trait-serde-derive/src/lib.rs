b870b7b21a7a5b488916f71cb3d84e9baf1225ca
Aditya Kresna <aditya.kresna@outlook.co.id>
2021-01-31 13:02:37 +0000

    Implement all derive for tapa-trait-serde

--- tapa-trait-serde-derive/src/lib.rs
+++ tapa-trait-serde-derive/src/lib.rs
@@ -1,7 +1,89 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn;
+
+#[proc_macro_derive(IBincodeSerializable)]
+pub fn macro_derive_ibincodeserializable(input: TokenStream) -> TokenStream {
+    let ast = syn::parse(input).unwrap();
+    impl_derive_ibincodeserializable(&ast)
+}
+
+fn impl_derive_ibincodeserializable(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = quote! {
+        impl IBincodeSerializable for #name {}
+    };
+    gen.into()
+}
+
+#[proc_macro_derive(ICborSerializable)]
+pub fn macro_derive_icborserializable(input: TokenStream) -> TokenStream {
+    let ast = syn::parse(input).unwrap();
+    impl_derive_icborserializable(&ast)
+}
+
+fn impl_derive_icborserializable(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = quote! {
+        impl ICborSerializable for #name {}
+    };
+    gen.into()
+}
+
+#[proc_macro_derive(IFlexbuffersSerializable)]
+pub fn macro_derive_iflexbuffersserializable(input: TokenStream) -> TokenStream {
+    let ast = syn::parse(input).unwrap();
+    impl_derive_iflexbuffersserializable(&ast)
+}
+
+fn impl_derive_iflexbuffersserializable(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = quote! {
+        impl IFlexbuffersSerializable for #name {}
+    };
+    gen.into()
+}
+
+#[proc_macro_derive(IJsonSerializable)]
+pub fn macro_derive_ijsonserializable(input: TokenStream) -> TokenStream {
+    let ast = syn::parse(input).unwrap();
+    impl_derive_ijsonserializable(&ast)
+}
+
+fn impl_derive_ijsonserializable(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = quote! {
+        impl IJsonSerializable for #name {}
+    };
+    gen.into()
+}
+
+#[proc_macro_derive(ITomlSerializable)]
+pub fn macro_derive_itomlserializable(input: TokenStream) -> TokenStream {
+    let ast = syn::parse(input).unwrap();
+    impl_derive_itomlserializable(&ast)
+}
+
+fn impl_derive_itomlserializable(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = quote! {
+        impl ITomlSerializable for #name {}
+    };
+    gen.into()
+}
+
+#[proc_macro_derive(IYamlSerializable)]
+pub fn macro_derive_iyamlserializable(input: TokenStream) -> TokenStream {
+    let ast = syn::parse(input).unwrap();
+    impl_derive_iyamlserializable(&ast)
+}
+
+fn impl_derive_iyamlserializable(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = quote! {
+        impl IYamlSerializable for #name {}
+    };
+    gen.into()
 }
