0430989970d96c0cc49c1503940a009185a0f3f5
Aditya Kresna <aditya.kresna@outlook.co.id>
2021-01-31 11:03:17 +0000

    Make default feature includes all

--- tapa-trait-serde/src/lib.rs
+++ tapa-trait-serde/src/lib.rs
@@ -1,13 +1,25 @@
+#[cfg(feature = "bincode")]
 mod bincode;
+#[cfg(feature = "cbor")]
 mod cbor;
+#[cfg(feature = "flexbuffers")]
 mod flexbuffers;
+#[cfg(feature = "json")]
 mod json;
+#[cfg(feature = "toml")]
 mod toml;
+#[cfg(feature = "yaml")]
 mod yaml;
 
+#[cfg(feature = "bincode")]
 pub use crate::bincode::IBincodeSerializable;
+#[cfg(feature = "cbor")]
 pub use crate::cbor::ICborSerializable;
+#[cfg(feature = "flexbuffers")]
 pub use crate::flexbuffers::IFlexbuffersSerializable;
+#[cfg(feature = "json")]
 pub use crate::json::IJsonSerializable;
+#[cfg(feature = "toml")]
 pub use crate::toml::ITomlSerializable;
+#[cfg(feature = "yaml")]
 pub use crate::yaml::IYamlSerializable;
