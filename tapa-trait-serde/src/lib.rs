c7cabcd39ebbcb6ab43af90018060d3f49e3fd67
Aditya Kresna <aditya.kresna@outlook.co.id>
2021-01-31 07:18:57 +0000

    Add yaml trait to tapa-trait-serde

--- tapa-trait-serde/src/lib.rs
+++ tapa-trait-serde/src/lib.rs
@@ -1,5 +1,7 @@
 mod bincode;
 mod json;
+mod yaml;
 
 pub use crate::bincode::IBincodeSerializable;
 pub use crate::json::IJsonSerializable;
+pub use crate::yaml::IYamlSerializable;
---
+++ tapa-trait-serde/src/yaml.rs
@@ -0,0 +1,25 @@
+use anyhow::Result as AnyResult;
+use serde::de::DeserializeOwned;
+use serde::Serialize;
+use serde_yaml::{from_slice, from_str, to_string, to_vec};
+
+pub trait IYamlSerializable<T = Self>
+where
+    Self: DeserializeOwned + Serialize + Clone + Send + Sized,
+{
+    fn from_yaml_string(yaml_string: &str) -> AnyResult<Self> {
+        Ok(from_str::<Self>(yaml_string)?)
+    }
+
+    fn from_yaml_slice(yaml_slice: &[u8]) -> AnyResult<Self> {
+        Ok(from_slice::<Self>(yaml_slice)?)
+    }
+
+    fn to_yaml_string(&self) -> String {
+        to_string(self).unwrap()
+    }
+
+    fn to_yaml_vec(&self) -> Vec<u8> {
+        to_vec(self).unwrap()
+    }
+}
