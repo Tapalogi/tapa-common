35e701f9559f3af60e1c78d27cb5e8784fd95c2c
Aditya Kresna <aditya.kresna@outlook.co.id>
2021-02-21 04:36:01 +0000

    Initial implementation of tapa-cgloop-nats

---
+++ tapa-cgloop-nats/README.md
@@ -0,0 +1,3 @@
+# Tapalogi's Consumer Group Loop for NATS
+
+Example usage is this [repo](https://github.com/Tapalogi/tapa-micro-mailer).
---
+++ tapa-cgloop-nats/src/lib.rs
@@ -0,0 +1,83 @@
+pub use anyhow::Result as AnyResult;
+pub use async_nats::{Message as NatsMessage, Options as NatsOptions};
+pub use futures::Future;
+
+use bytes::Bytes;
+use log::debug;
+use std::pin::Pin;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+
+pub enum ProcessResult {
+    Success(Bytes),
+    Failure(Bytes),
+}
+
+pub type NatsMessageHandler =
+    &'static fn(&NatsMessage) -> Pin<Box<dyn Future<Output = AnyResult<ProcessResult>>>>;
+
+pub struct CGLoop {
+    pub url: String,
+    pub source_topic: String,
+    pub success_topic: String,
+    pub failure_topic: String,
+    pub group: String,
+    pub unsub_on_exit: bool,
+}
+
+impl CGLoop {
+    pub fn new(
+        url: &str,
+        source_topic: &str,
+        success_topic: &str,
+        failure_topic: &str,
+        group: &str,
+        unsub_on_exit: bool,
+    ) -> Self {
+        Self {
+            url: url.into(),
+            source_topic: source_topic.into(),
+            success_topic: success_topic.into(),
+            failure_topic: failure_topic.into(),
+            group: group.into(),
+            unsub_on_exit,
+        }
+    }
+
+    pub async fn run(
+        self,
+        options: NatsOptions,
+        shutdown_flag: Arc<AtomicBool>,
+        on_message: NatsMessageHandler,
+    ) -> AnyResult<()> {
+        let connection = options.connect(&self.url).await?;
+        let subscription = connection.queue_subscribe(&self.source_topic, &self.group).await?;
+
+        while !shutdown_flag.load(Ordering::Relaxed) {
+            if let Some(received_message) = subscription.next().await {
+                match (on_message)(&received_message).await? {
+                    ProcessResult::Failure(failure_message) => {
+                        connection.publish(&self.failure_topic, &failure_message).await?;
+                        debug!(
+                            "Failure message sent to topic {} with size {} bytes",
+                            &self.failure_topic,
+                            failure_message.len()
+                        );
+                    }
+                    ProcessResult::Success(success_message) => {
+                        connection.publish(&self.success_topic, &success_message).await?;
+                        debug!(
+                            "Failure message sent to topic {} with size {} bytes",
+                            &self.success_topic,
+                            success_message.len()
+                        );
+                    }
+                }
+            }
+        }
+
+        debug!("Loop exited");
+
+        Ok(())
+    }
+}
